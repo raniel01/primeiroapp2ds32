07ea1ab27c57e7d28134378a839c1f19057da2ac
jussimar <jussimar@jussimarleal.com.br>
2019-08-06 07:25:47 +0900

    alterações até o inicio da aula 05/08/2019

--- www/components/acao.js
+++ www/components/acao.js
@@ -1,2 +1,23 @@
 // This is a JavaScript file
+function retorno(){
+
+}
+
+function retorno2(buttonIndex) {
+    if(buttonIndex == 1){
+      navigator.notification.alert("Sim");
+      navigator.notification.beep(1);
+    }else{
+       navigator.notification.alert("Não");
+       navigator.notification.beep(2);
+    }
+    
+}
+
+$(document).on("click","#alerta",function(){
+    //navigator.notification.alert("Olá Mundo - Primeiro App!",retorno,"Aviso!","ok!");
+    //navigator.notification.beep(2);
+    
+    navigator.notification.confirm("Escolha uma opção!",retorno2,"Titulo",['Sim','Não']);
+});
 
